ea1414a20f41b242b9ecd8b4444b6759f8c677ba
Prince kyei <prince.kaf12@gmail.com>
2024-04-10 17:11:10 +0200

    added

--- server/routes/posts.js
+++ server/routes/posts.js
@@ -1,14 +1,14 @@
-import express from 'express';
-import { getUserFeedPosts, getUserPosts, likePost } from "../controllers/posts.js";
-import { verifyToken } from '../middleware/auth';
+import express from "express";
+import { getFeedPosts, getUserPosts, likePost } from "../controllers/posts.js";
+import { verifyToken } from "../middleware/auth.js";
 
 const router = express.Router();
 
-//READ
-router.get("/", verifyToken, getUserFeedPosts);
+/* READ */
+router.get("/", verifyToken, getFeedPosts);
 router.get("/:userId/posts", verifyToken, getUserPosts);
 
-//UPDATE
-router.patch('/:id/like', verifyToken, likePost);
+/* UPDATE */
+router.patch("/:id/like", verifyToken, likePost);
 
 export default router;
